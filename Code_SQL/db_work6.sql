698a081d3e6f41b354b3d61113486c9b9d42af96
fufupao <ffffuman@163.com>
2025-06-01 16:56:56 +0800

    update file

--- Code_SQL/db_work6.sql
+++
@@ -1,6 +0,0 @@
----1.1 使用join得到每个学院的名字（dept_name）及每个学院的老师总数。
-SELECT department.dept_name, COUNT(id) as total_instructor
-FROM department
-LEFT JOIN instructor
-ON department.dept_name = instructor.dept_name
-GROUP BY department.dept_name ;
